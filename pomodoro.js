c8c9856ce55bfda0d68b973612f6532764a6d927
lfdo20 <lfdo20@gmail.com>
2017-10-15 13:44:07 -0200

    Functional and Styled

--- pomodoro.js
+++ pomodoro.js
@@ -1,60 +1,39 @@
 $('document').ready(function(){
 
-  var pause = 2;
-  var work = 1;
+  var pause = 5;
+  var work = 25;
   var loop = 1;
   var looprb = 1;
-  var oldwork, oldpause;
-/*
-  var dt = new Date();
-  var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-console.log(time);
+  var oldwork=0;
+  var oldpause=0;
+  var timer =0;
 
-var date = new Date(new Date().valueOf();
-console.log(date);
-
-
-*/
 
   $(".display").click(function() {
       if (loop ===1){
-        loop = 0;
-        $(".fa").css({'color' : 'green',
+        loop=0;
+        $(".fa-2x").css({'color' : '#d28521',
         "cursor": "wait",
         "pointer-events": "none"
         });
           if(oldwork !== work || oldpause !== pause){
-           runwork();
-            /*
-              var date = new Date(new Date().valueOf() + work * 60 * 1000);
-              console.log(date);
-              $('.counter').countdown(date, function(event) {
-              $(this).html(event.strftime('%M:%S'));
-            });
-
-            do {
-              if (looprb === 1){
-
-
-              }
-              else if (looprb === 0){
-
-              }
-            } while (loop === 1); */
+            looprb=1;
+            $(".clock").css({'borderColor' : '#35550F'});
+            runwork();
           }
           else{
-              $('.counter').countdown('resume');
+              timer.start();
       }
       }
       else{
-        loop = 1;
-        $(".fa").css({'color' : 'black',
+        $(".fa-2x").css({'color' : '#d8dccd;',
         "cursor": "pointer",
         "pointer-events": "auto",
       });
       oldwork = work;
       oldpause = pause;
-      $('.counter').countdown('pause');
+      timer.pause();
+      loop = 1;
       }
       console.log(loop);
     });
@@ -69,7 +48,7 @@ console.log(date);
       });
 
       $(".pausedown").click(function(){
-        if (pause > 1) {pause = pause-1;
+        if (pause > 2) {pause = pause-1;
         $(".setpause").html(pause);}
       });
 
@@ -80,7 +59,7 @@ console.log(date);
       });
 
       $(".workdown").click(function(){
-        if (work > 1) { work = work-1;
+        if (work > 10) { work = work-1;
         $(".setwork").html(work);}
       });
     }
@@ -90,32 +69,62 @@ console.log(date);
 
 
 function runwork(){
-  console.log('work' + work);
-  var date = new Date(new Date().valueOf() + work * 5 * 1000);
-  console.log(date);
-  $('.counter').countdown(date, function(event) {
-    $(this).html(event.strftime('%M:%S'))
-    .on('finish.countdown', function(event){
-
-      $(".clock").css({'borderColor' : 'green'});
-       runpause();
+  timer = new Timer();
+  var spin=0;
+  timer.start({countdown: true, startValues: {seconds: (work*60)}});
+  $('.counter').html(timer.getTimeValues().toString(['minutes', 'seconds']));
+    console.log('work' + work);
+    timer.addEventListener('secondsUpdated', function (e) {
+      $('.counter').html(timer.getTimeValues().toString(['minutes', 'seconds']));
+      spin = spin+ ((360/(work*60)));
+      console.log(spin);
+      $(".arrow2").css({
+          '-moz-transform':'rotate('+spin+'deg)',
+          '-webkit-transform':'rotate('+spin+'deg)',
+          '-o-transform':'rotate('+spin+'deg)',
+          '-ms-transform':'rotate('+spin+'deg)',
+          'transform': 'rotate('+spin+'deg)'
+     });
     });
-  });
-}
+    timer.addEventListener('targetAchieved', function (e) {
+        $(".clock").css({'borderColor' : '#ad2a47'});
+        looprb = 0;
+        console.log('Fim trabalho'+looprb);
+        timer.stop();
+        timer =0;
+        runpause();
+    });
+  };
+
 
 function runpause(){
-  console.log('pause' +pause);
-  var date = new Date(new Date().valueOf() + pause * 5 * 1000);
-  console.log(date);
-  $('.counter').countdown(date, function(event) {
-    $(this).html(event.strftime('%M:%S'))
-    .on('finish.countdown', function(event){
-
-      $(".clock").css({'borderColor' : 'red'});
-      runwork();
+  timer = new Timer();
+  var spin=0;
+  timer.start({countdown: true, startValues: {seconds: (pause*60)}});
+  $('.counter').html(timer.getTimeValues().toString(['minutes', 'seconds']));
+    console.log('pause' + pause);
+    timer.addEventListener('secondsUpdated', function (e) {
+      $('.counter').html(timer.getTimeValues().toString(['minutes', 'seconds']));
+      spin = spin+ ((360/(pause*60)));
+      console.log(spin);
+      $(".arrow2").css({
+          '-moz-transform':'rotate('+spin+'deg)',
+          '-webkit-transform':'rotate('+spin+'deg)',
+          '-o-transform':'rotate('+spin+'deg)',
+          '-ms-transform':'rotate('+spin+'deg)',
+          'transform': 'rotate('+spin+'deg)'
+     });
     });
-  });
-}
+    timer.addEventListener('targetAchieved', function (e) {
+        $(".clock").css({'borderColor' : 'green'});
+        looprb = 1;
+        console.log('Fim descanso'+looprb);
+        timer.stop();
+        timer=0;
+        runwork();
+    });
+  };
+
 
 /*
   console.log(work);
